f6da24f0f650e8874e1e15fae53a8556e07030dd
Rhys Powell <rhys@rpowell.me>
2013-07-22 23:46:09 +1000

    Added message item template, room now shows messages

--- client/templates/rooms/room_page.js
+++ client/templates/rooms/room_page.js
@@ -1,5 +1,8 @@
 Template.roomPage.helpers({
   currentRoom: function() {
     return Rooms.find({_id: Session.get('currentRoomId')})
+  },
+  messages: function() {
+    return Messages.find({roomId: Session.get('currentRoomId')})
   }
 })
