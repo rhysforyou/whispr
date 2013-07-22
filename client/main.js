5873061b81b6a0545fe02f1ccbeecf2419bd2acb
Rhys Powell <rhys@rpowell.me>
2013-07-22 23:43:57 +1000

    Created and subscribed to publication for current room's messages

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
 Deps.autorun(function() {
   Meteor.subscribe('currentRoom', Session.get('currentRoomId'))
+  Meteor.subscribe('currentRoomMessages', Session.get('currentRoomId'))
 })
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
-Meteor.publish('currentRoom', function(roomId) {
+Meteor.publish("currentRoom", function(roomId) {
   return Rooms.find({_id: roomId})
+})
+
+Meteor.publish("currentRoomMessages", function(roomId) {
+  return Messages.find({roomId: roomId})
 })
