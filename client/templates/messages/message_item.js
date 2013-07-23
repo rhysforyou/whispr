8caa45f5a4437da9ee3885a82a837e37087b4af1
Rhys Powell <rhys@rpowell.me>
2013-07-23 19:16:03 +1000

    Styling for user messages

--- client/templates/messages/message_item.js
+++ client/templates/messages/message_item.js
@@ -1,5 +1,8 @@
 Template.messageItem.helpers({
   author: function() {
     return Meteor.users.findOne(this.userId).profile.nickname
+  },
+  ownerClass: function() {
+    return this.userId === Meteor.userId() ? 'user' : ''
   }
 })
