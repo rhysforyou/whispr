a6785dfbfd613ae56c6aa617cabaab168424c7ec
Rhys Powell <rhys@rpowell.me>
2013-07-23 14:05:56 +1000

    Tie a user's ID to any messages they post

--- collections/messages.js
+++ collections/messages.js
@@ -3,15 +3,19 @@ Messages = new Meteor.Collection("messages")
 Meteor.methods({
   newMessage: function(message) {
     var room = Rooms.findOne({_id: message.roomId})
+    var user = Meteor.user()
 
     if (!message.body)
       throw new Meteor.Error(400, "Messages need a body")
     if (!room)
       throw new Meteor.Error(400, "Messages must be posted to a valid room")
+    if (!user)
+      throw new Meteor.Error(401, "You must be signed in to do that")
 
     message = _.extend(_.pick(message, "body"), {
       createdAt: new Date().getTime(),
-      roomId: room._id
+      roomId: room._id,
+      userId: user._id
     })
 
     message._id = Messages.insert(message)
