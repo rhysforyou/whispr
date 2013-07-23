96c0f2b9ed6f61c155debb9bebcf61a70cbdff69
Rhys Powell <rhys@rpowell.me>
2013-07-23 18:53:46 +1000

    Improved event handling for message bar

--- client/templates/messages/message_compose.js
+++ client/templates/messages/message_compose.js
@@ -1,20 +1,29 @@
 Template.messageCompose.events({
-  'submit form#new-message': function(event) {
+  "submit form#new-message": function(event) {
     event.preventDefault()
-
-    var body = $(event.target).find(".body").val()
-
-    var message = {
-      body: body,
-      roomId: Session.get('currentRoomId')
+    submitForm()
+  },
+  "keydown": function(event) {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      event.preventDefault()
+      submitForm()
     }
-
-    Meteor.call('newMessage', message, function(error) {
-      // Super crude error reporting
-      if (error)
-        alert(error.message)
-    })
-
-    $(event.target).find(".body").val('')
   }
 })
+
+var submitForm = function() {
+  var body = $("#new-message .body").val()
+
+  var message = {
+    body: body,
+    roomId: Session.get("currentRoomId")
+  }
+
+  Meteor.call("newMessage", message, function(error) {
+    // Super crude error reporting
+    if (error)
+      alert(error.message)
+  })
+
+  $("#new-message .body").val("")
+}
