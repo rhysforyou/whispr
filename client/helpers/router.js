63ae0b7e6d999cf79b0d506e5e0ce9e4eb9a0f2a
Rhys Powell <rhys@rpowell.me>
2013-07-23 20:10:00 +1000

    Keep track of rooms that users enter

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -2,6 +2,14 @@ Meteor.Router.add({
   '/': 'home',
   '/:_id': {
     to: 'roomPage',
-    and: function(roomId) { Session.set('currentRoomId', roomId) }
+    and: function(roomId) {
+      Session.set('currentRoomId', roomId)
+
+      if (Meteor.user()) {
+        Meteor.users.update(Meteor.userId(), {
+          $addToSet: {"profile.roomIds": roomId}
+        })
+      }
+    }
   }
 })
